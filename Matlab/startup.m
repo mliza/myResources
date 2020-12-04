6c188122aafe3c4f0cd7ac41bc81ea6d2063c3a2
Martin E. Liza <mliza@email.arizona.edu>
2020-12-04 16:00:15 -0700

    change some property

--- Matlab/startup.m
+++ Matlab/startup.m
@@ -1,13 +1,13 @@
 format long g
 addpath(genpath('/Users/Martin/Documents/Documents/gitRepo/Scripts/'));
 whitebg('black');
-set(groot, 'defaultLineLineWidth', 2.5);
+set(groot, 'defaultLineLineWidth', 3);
 set(groot, 'DefaultAxesXGrid', 'on');
 set(groot, 'DefaultAxesYGrid', 'on');
 set(groot, 'DefaultAxesGridAlpha', 0.5);
 set(groot, 'DefaultAxesGridLineStyle', '-.');
 set(groot, 'DefaultAxesGridColor', 'r');
-set(groot, 'DefaultAxesFontSize', 10);
+set(groot, 'DefaultAxesFontSize', 12.5);
 set(groot, 'DefaultAxesFontWeight', 'bold')
 set(gcf, 'InvertHardcopy', 'off'); 
 UnitSystem='UnitSystemCode';  %units plugin
