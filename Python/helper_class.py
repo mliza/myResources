14613796b19f20a2266c79e8dd7dd5c0d3a7c3e2
Martin E. Liza <mliza@email.arizona.edu>
2022-07-18 22:41:10 -0700

    Added dict_pickle_add

--- Python/helper_class.py
+++ Python/helper_class.py
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/python3
 '''
-    Date:   05/23/2022
+    Date:   07/18/2022
     Author: Martin E. Liza
     File:   helper_class.py
     Def:    Contains some helper functions 
@@ -9,10 +9,12 @@
     Author		    Date		Revision
     ----------------------------------------------------
     Martin E. Liza	05/23/2022	Initial version.
+    Martin E. Liza	07/18/2022	Added pickle_dict_add.
 '''
 import os 
 import pickle 
 import numpy as np 
+import IPython 
 from scipy.io import FortranFile  
 from dataclasses import dataclass
 
@@ -29,15 +31,26 @@ class Helper:
         return data
 
 # Save data as pickle and loads data as a pickle 
-    def pickle_manager(self, pickle_name, pickle_path, data_in=None):
-        # Loads pickle file if data_in is empty  
-        if data_in is None:  
-            file_in   = os.path.join(f'{pickle_path}',f'{pickle_name}.pickle') 
+    def pickle_manager(self, pickle_name_file, pickle_path, data_to_save=None):
+        # Loads pickle file if data_to_save is empty  
+        if data_to_save is None:  
+            file_in   = os.path.join(f'{pickle_path}',f'{pickle_name_file}.pickle') 
             pickle_in = open(file_in, 'rb')
             return pickle.load(pickle_in)
         # Creates pickle file if data_in has a path and saves it in that path  
         else:
-            file_out   = os.path.join(f'{pickle_path}',f'{pickle_name}.pickle') 
+            file_out   = os.path.join(f'{pickle_path}',f'{pickle_name_file}.pickle') 
             pickle_out = open(file_out, 'wb') 
-            pickle.dump(data_in, pickle_out)
+            pickle.dump(data_to_save, pickle_out)
             pickle_out.close() 
+
+# Opens pickle dictionary, adds a variable and saves it
+    def pickle_dict_add(self, var_in, var_in_str, pickle_path, 
+                        pickle_dict_in, pickle_dict_out):
+        # Open pickle dictionary 
+        dict_in = self.pickle_manager(pickle_dict_in, pickle_path) 
+        dict_in[var_in_str] = var_in
+        self.pickle_manager(pickle_dict_out, pickle_path, dict_in)
+
+
+
