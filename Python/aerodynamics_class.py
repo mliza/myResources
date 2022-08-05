3b54a7b23a587b929101d965b857d81e1bc8a5e4
Martin E. Liza <mliza@email.arizona.edu>
2022-08-04 17:26:14 -0700

    added oblique shock realtions

--- Python/aerodynamics_class.py
+++ Python/aerodynamics_class.py
@@ -8,8 +8,8 @@
     Author		    Date		Revision
     --------------------------------------------------------
     Martin E. Liza	07/25/2022	Initial version.
-    Martin E. Liza	08/04/2022	Added normal shock 
-                                relations function. 
+    Martin E. Liza	08/04/2022	Added normal and oblique 
+                                shock relation functions. 
 
 '''
 import os 
@@ -23,7 +23,7 @@ import IPython
 class Aero:  
     pass 
 
-# Sutherland Law
+# Sutherland law
     def sutherland_law(self, temperature_field_K): 
         # https://doc.comsol.com/5.5/doc/com.comsol.help.cfd/cfd_ug_fluidflow_high_mach.08.27.html
         mu_ref  = 1.716e-5 # [kg/m*s] 
@@ -33,7 +33,7 @@ class Aero:
                         (T_ref + S_const) / (temperature_field_K + S_const) ) )
         return mu #[kg/m*s] 
 
-# Speed of Sound 
+# Speed of sound 
     def speed_of_sound(self, temperature_field_K):
         gas_const = s_consts.R                  # [J/mol*K]
         gamma     = 1.4                         # [ ] 
@@ -49,7 +49,7 @@ class Aero:
                                gas_const / dry_air_m) # [m/s]
         return spd_of_sound #[m/s]
 
-# Turbulent Mach number
+# Turbulent mach number
     def turbulent_mach_number(self, kinetic_energy, speed_of_sound): 
         kinetic_energy_fluctuation = (kinetic_energy - 
                                       np.mean(kinetic_energy))   # [m^2 / s^2]
@@ -63,18 +63,49 @@ class Aero:
     def normal_shock_relations(self, mach_1):
         # https://www.grc.nasa.gov/www/k-12/airplane/normal.html
         # Note ratio = var_1 / var_2
-        gamma   = 1.4                                        # [ ] 
+        gamma   = 1.4                                      
         mach_2  = np.sqrt( ((gamma - 1) * mach_1**2 + 2) / 
                            (2 * gamma * mach_1**2 - (gamma - 1)) )
         P_ratio = ( (2 * gamma / (gamma + 1)) * mach_1**2 - 
                     (gamma - 1) / (gamma + 1) ) 
         T_ratio = ( ((2 * gamma * mach_1**2 - (gamma - 1)) * 
-                ((gamma - 1) * mach_1**2 + 2)) / ((gamma + 1)**2 * mach_1**2) )
+            ((gamma - 1) * mach_1**2 + 2)) / ((gamma + 1)**2 * mach_1**2) )
         R_ratio = ( ((gamma + 1) * mach_1**2) / 
                     ((gamma - 1) * mach_1**2 + 2) )
         # Return Dictionary 
-        shock_relations_dict = { 'mach_2'    : mach_2,
-                                 'P_ratio'   : P_ratio, 
-                                 'T_ratio'   : T_ratio,
-                                 'Rho_ratio' : R_ratio }
-        return shock_relations_dict
+        normal_shock_dict = { 'mach_2'    : mach_2,
+                              'P_ratio'   : P_ratio, 
+                              'T_ratio'   : T_ratio,
+                              'Rho_ratio' : R_ratio }
+        return normal_shock_dict
+
+# Oblique shock relations
+    def oblique_shock_relations(self, mach_1, shock_angle_deg):
+    # REF : Modern Compressible Flows With Historical Ref., eq 4.7 - 4.11 
+    # NOTE: Equations only work for weak shocks 
+    # Note ratio = var_1 / var_2
+        gamma       = 1.4                                        
+        shock_angle = np.radians(shock_angle_deg)
+        sin2_shock  = np.sin(shock_angle)**2
+        mach_n1     = mach_1 * np.sin(shock_angle) #normal mach number 
+        # Calculates Deflection angle 
+        deflection_angle = ( np.tan(shock_angle) * (
+                          ((gamma + 1) * mach_1**2) / 
+                          (2 * (mach_n1**2  - 1)) - 1) ) 
+        deflection_angle_deg = np.degrees(np.arctan(1 / deflection_angle)) 
+        # Calculates properties downstream the shock  
+        R_ratio = ( ((gamma + 1) * mach_n1**2) / 
+                    ((gamma - 1) * mach_n1**2 + 2) )
+        P_ratio = 1 + 2 * gamma * (mach_n1**2 - 1) / (gamma + 1) 
+        T_ratio = P_ratio * 1 / R_ratio 
+        # Calculates mach 2 
+        mach_n2 = np.sqrt( (mach_n1**2 + (2 / (gamma - 1))) / 
+                    ((2 * gamma / (gamma - 1)) * mach_n1**2 - 1) )
+        mach_2  = mach_n2 / np.sin(np.radians(shock_angle_deg - 
+                                  deflection_angle_deg)) 
+        # Dictionary 
+        oblique_shock_dict = { 'mach_2'    : mach_2,
+                               'P_ratio'   : P_ratio, 
+                               'T_ratio'   : T_ratio,
+                               'Rho_ratio' : R_ratio }
+        return oblique_shock_dict
