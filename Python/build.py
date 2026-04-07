a71ab2505e9b14823289d73989b17b27ea405434
Martin E. Liza <mliza@email.arizona.edu>
2026-04-07 10:12:34 -0400

    upadetd

--- Python/build.py
+++ Python/build.py
@@ -1,69 +1,69 @@
 #!/opt/homebrew/bin/python3
 """
-   Date:    10/20/2019 
-   Author:  Martin E. Liza 
-   File:    build.py 
-   Def:     Allows to compile C++, LaTeX, C and Fortran 
-            codes by specifying the proper flags 
-      
-   Available flags: 
-   -tex = LaTeX compiler 
-   -cpp = C++ compiler 
-   -c   = C compiler 
-   -f   = Fortran compiler 
+   Date:    10/20/2019
+   Author:  Martin E. Liza
+   File:    build.py
+   Def:     Allows to compile C++, LaTeX, C and Fortran
+            codes by specifying the proper flags
+
+   Available flags:
+   -tex = LaTeX compiler
+   -cpp = C++ compiler
+   -c   = C compiler
+   -f   = Fortran compiler
 
    ex: ./build -flag myCode.*
-        
 
-   Author             Date           Revision  
+
+   Author             Date           Revision
    --------------------------------------------------
-   Martin E. Liza     10/20/2019     Initial Version  
-   Martin E. Liza     05/06/2021     Updated compiler versions 
-""" 
-import argparse 
-import subprocess 
+   Martin E. Liza     10/20/2019     Initial Version
+   Martin E. Liza     05/06/2021     Updated compiler versions
+"""
+import argparse
+import subprocess
 
 #Ex. build -cpp "myFile.cpp myFile.hpp"
-#NOTE: Requires, g++-9, pdflatex, gcc-9 and gfortran-9  
+#NOTE: Requires, g++-9, pdflatex, gcc-9 and gfortran-9
 
-#Available compilers  
-cppCopiler = 'g++-11 -std=c++17' 
-texCopiler = 'pdflatex' 
-cCopiler   = 'gcc-11' 
-fCopiler   = 'gfortran' 
+#Available compilers
+cppCopiler = 'g++-11 -std=c++17'
+texCopiler = 'pdflatex'
+cCopiler   = 'gcc-11'
+fCopiler   = 'gfortran'
 
 #Make parser object
-parser = argparse.ArgumentParser() 
+parser = argparse.ArgumentParser()
 
-#Compiler selection flags (helper)  
-parser.add_argument('-cpp', action='store_true', 
+#Compiler selection flags (helper)
+parser.add_argument('-cpp', action='store_true',
                              help='C++ compiler')
 
 parser.add_argument('-tex', action='store_true',
-                           help='LaTeX compiler') 
+                           help='LaTeX compiler')
 
 parser.add_argument('-c', action='store_true',
                              help='C compiler')
 
 parser.add_argument('-f', action='store_true',
                        help='Fortran compiler')
-#Add specific compiler arguments in quotes   
+#Add specific compiler arguments in quotes
 parser.add_argument('flags', nargs='*' )
 
-#Needed for if statements and for building shell command   
+#Needed for if statements and for building shell command
 args  = parser.parse_args()
-flags = (','.join(args.flags))  
-flags = flags.replace(',', ' ') 
+flags = (','.join(args.flags))
+flags = flags.replace(',', ' ')
 
-#Running commands 
-if args.cpp: 
+#Running commands
+if args.cpp:
     cmd = f'{cppCopiler} {flags}'
-if args.tex: 
-    cmd = f'{texCopiler} {flags}' 
-if args.c: 
+if args.tex:
+    cmd = f'{texCopiler} {flags}'
+if args.c:
     cmd = f'{cCopiler} {flags}'
-if args.f: 
-    cmd = f'{fCopiler} {flags}' 
+if args.f:
+    cmd = f'{fCopiler} {flags}'
 
-#Running Shell 
-subprocess.call([cmd], shell=True) 
+#Running Shell
+subprocess.call([cmd], shell=True)
